26512e547f6a85ccf147796c5f433f67345983c7
RodrigoTravessa <rodrigosigiliao@outlook.com>
2023-12-13 16:43:16 -0300

    Removed DropDown From Nav

---
+++ MyPortfolio/Pages/Contato.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyPortfolio.Pages
+{
+    public class ContatoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MyPortfolio/Pages/Sobre.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyPortfolio.Pages
+{
+    public class SobreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
